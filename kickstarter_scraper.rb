58158050ab12552fcda74a37fbbac13bc448bb40
Ingrid Wong <yw340@georgetown.edu>
2018-01-22 01:09:41 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -25,8 +25,7 @@
 require 'nokogiri'
 require 'pry'
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
   projects = {}
 
   kickstarter.each do |project|
